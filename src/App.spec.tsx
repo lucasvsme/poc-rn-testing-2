dfdc0f7f04af32893826f0cdcf6f2d42c065d745
Lucas <lucasvalenteds@gmail.com>
2020-01-18 12:14:32 -0300

    Rewrite test file

--- src/App.spec.tsx
+++ src/App.spec.tsx
@@ -1,9 +1,16 @@
-import 'react-native';
+import {Text} from 'react-native';
 import React from 'react';
 import App from './App';
 
-import renderer from 'react-test-renderer';
+import ReactRenderer, {
+  ReactTestRenderer,
+  ReactTestInstance,
+} from 'react-test-renderer';
 
-test('renders correctly', () => {
-  renderer.create(<App />);
+test('It renders the text', () => {
+  const renderer: ReactTestRenderer = ReactRenderer.create(<App />);
+
+  const node: ReactTestInstance = renderer.root.findByType(Text);
+
+  expect(node.props.children).toStrictEqual('React Native');
 });
